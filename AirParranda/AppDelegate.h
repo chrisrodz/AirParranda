d1da85aa41532d0c2fa49848d8462caad50a3406
Christian <christian.etpr10@gmail.com>
2014-09-27 16:55:56 -0400

    Initial Commit

---
+++ AirParranda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirParranda
+//
+//  Created by Christian A. Rodriguez on 9/27/14.
+//  Copyright (c) 2014 Mai Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AirParranda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirParranda
+//
+//  Created by Christian A. Rodriguez on 9/27/14.
+//  Copyright (c) 2014 Mai Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
